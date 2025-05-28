1d7de67439f9b5c815b86feb37ad610916bd1fd3
bmaarianto <bimaarianto19@gmail.com>
2025-05-28 21:37:27 +0700

    Refactor: use yargs for CLI input and modularize contact saving logic

--- contacts.js
+++ contacts.js
@@ -1,10 +1,4 @@
 const fs = require("fs");
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 const dirPath = "./data";
 if (!fs.existsSync(dirPath)) {
@@ -16,25 +10,25 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-const askQuestion = (question) => {
-  return new Promise((resolve, reject) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
-  });
-};
-
 const saveContact = (name, email, phoneNumber) => {
   const contact = { name, email, phoneNumber };
   const fileBuffer = fs.readFileSync("data/contacts.json", "utf-8");
   const contacts = JSON.parse(fileBuffer);
+
+  const duplicate = contacts.find(
+    (contact) => contact.phoneNumber === phoneNumber
+  );
+
+  if (duplicate) {
+    console.log("kontak sudah terdaftar");
+    return false;
+  }
 
   contacts.push(contact);
 
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
 
   console.log("Terima kasih sudah memasukkan data.");
-  rl.close();
-}
+};
 
-module.exports = {askQuestion, saveContact}
+module.exports = {saveContact };
